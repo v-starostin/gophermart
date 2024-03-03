19f79e406d7dcc60e18337f8bc742eaf61f139d3
Viacheslav Starostin <viacheslavstarostin1@yandex.kz>
2024-03-03 16:49:07 +0500

    Create DB tables

--- db/migrations/0_init.up.sql
+++ db/migrations/0_init.up.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS users (
     id uuid NOT NULL,
     login varchar NOT NULL,
     password varchar NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id),
+    UNIQUE (login)
 );
 
 CREATE TABLE IF NOT EXISTS tokens (
@@ -10,4 +11,34 @@ CREATE TABLE IF NOT EXISTS tokens (
     user_id uuid NOT NULL UNIQUE REFERENCES users(id) ON DELETE CASCADE,
     token varchar NOT NULL,
     PRIMARY KEY(id)
-)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    id uuid NOT NULL,
+    order_number varchar UNIQUE NOT NULL,
+    user_id uuid UNIQUE NOT NULL REFERENCES users(id),
+    status varchar NOT NULL,
+    accrual integer,
+    uploaded_at timestamp DEFAULT CURRENT_TIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS withdraws (
+    id uuid NOT NULL,
+    order_id uuid UNIQUE NOT NULL REFERENCES orders(id),
+    user_id uuid UNIQUE NOT NULL REFERENCES users(id),
+    status varchar NOT NULL,
+    accrual integer,
+    processed_at timestamp DEFAULT CURRENT_TIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS accruals (
+     id uuid NOT NULL,
+     order_id uuid UNIQUE NOT NULL REFERENCES orders(id),
+     user_id uuid UNIQUE NOT NULL REFERENCES users(id),
+     status varchar NOT NULL,
+     accrual integer,
+     processed_at timestamp DEFAULT CURRENT_TIME,
+     PRIMARY KEY (id)
+);
